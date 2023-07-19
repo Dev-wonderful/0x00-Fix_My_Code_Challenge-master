e052ea24b74c1e488f0e851bd419294884979c77
Dev-wonderful <adeonderful20@gmail.com>
2023-07-19 12:42:17 +0100

    square correction

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
+"""Define a base square class"""
 
-class square():
-    
+class Square():
+    """Square class"""
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """Instantiate an instance"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def perimeter_of_my_square(self):
+    def permiter_of_my_square(self):
+        """Calculate Perimeter"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """User friendly output"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.permiter_of_my_square())
